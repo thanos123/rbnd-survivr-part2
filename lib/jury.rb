091d056912b5940688db0c7722d29a58abdfbacf
Thanos Atskakanis <thanosatskakanis@gmail.com>
2016-05-17 15:24:25 +0100

    dev for jury tests

---
+++ lib/jury.rb
@@ -0,0 +1,31 @@
+class Jury
+  attr_accessor :members
+
+  def initialize
+    @members = []
+  end
+
+  def add_member(member)
+    @members << member
+  end
+
+  def cast_votes(finalists)
+    final_votes = Hash[finalists.collect{ |finalist| [finalist, 0] }]
+    @members.each do |member|
+      finalist = finalists.sample
+      puts "#{member} votes for #{finalist}"
+      final_votes[finalist] += 1
+    end
+    final_votes
+  end
+
+  def report_votes(final_votes)
+    final_votes.each do |finalist, votes|
+      puts "#{votes} votes for #{finalist}"
+    end
+  end
+
+  def announce_winner(final_votes)
+    final_votes.key(final_votes.values.max)
+  end
+end
